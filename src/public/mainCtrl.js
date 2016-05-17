47f12a7ad39ea3b07544166fa9b12f69a4486e4e
Joel Chacon <taktic11@gmail.com>
2016-05-17 18:27:35 -0500

    d

--- src/public/mainCtrl.js
+++ src/public/mainCtrl.js
@@ -13,9 +13,18 @@ angular.module('app').controller('mainCtrl', function($scope, myFactory){
 		return $scope.visible = $scope.visible ? false : true;
 	};
 	//practice for the $q library
-	function add(x, y) {
-		return(x + y);
+	function add(x, y, callback) {
+		callback(x + y);
 	};
-	$scope.result = add(5, 8);
+	add(34, 3234, function(joel) {
+		$scope.result = joel;
+	});
+
+	function subtract(e, i, callback) {
+		callback(e * i);
+	};
+	subtract(23, 98, function(theResult) {
+		$scope.anjel = theResult;
+	});
 
 });
