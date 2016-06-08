3498c972a54b9d80e808c0804457fb893abde64f
Joel Chacon <taktic11@gmail.com>
2016-06-08 15:37:31 -0500

    new pic

--- src/public/mainCtrl.js
+++ src/public/mainCtrl.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('app').controller('mainCtrl', function($scope, myFactory){
 
 	$scope.name = "Joel Chacon";
@@ -5,26 +6,30 @@ angular.module('app').controller('mainCtrl', function($scope, myFactory){
 	$scope.car = myFactory.getCar();
 	//mijo's info
 	$scope.mijosInfo = myFactory.getMijosInfo();
+	
 	//ng-click
 	 //This will hide the DIV by default.
-	$scope.visible = true;
 	$scope.showAndHide = function() {
+		$scope.visible = true;
 		//If DIV is visible it will be hidden and vice versa.
-		return $scope.visible = $scope.visible ? false : true;
-	};
+		 $scope.visible = $scope.visible ? false : true;
+	}
 	//practice for the $q library
-	function add(x, y, callback) {
+	function add(x, y, callback) { // step 1. the function is created by me
 		callback(x + y);
-	};
+	}
 	add(34, 3234, function(joel) {
 		$scope.result = joel;
-	});
+	})
 
 	function subtract(e, i, callback) {
 		callback(e * i);
-	};
+	}
 	subtract(23, 98, function(theResult) {
 		$scope.anjel = theResult;
 	});
-
 });
+ // encaptialation
+ // functional programming
+ // object oriented programming
+	visi
